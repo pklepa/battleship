dee14fdefbb3de72e4dc89d80ec2186302a38e6a
klepa <pedro.klepa@gmail.com>
2020-08-27 11:25:01 -0300

    Slight modifications to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,8 @@ import { ReactComponent as Logo } from "../../assets/images/port.svg";
 
 function Header() {
   const style = {
-    width: 60,
-    height: 60,
+    width: 45,
+    height: 45,
     backgroundColor: "fdcb9e",
     borderRadius: 50,
     padding: 5,
@@ -14,8 +14,10 @@ function Header() {
 
   return (
     <header>
-      <Logo style={style} fill="#1b262c" />
-      <h1>battleship</h1>
+      <div className="logo">
+        <Logo style={style} fill="#1b262c" />
+        <h1>battleship</h1>
+      </div>
     </header>
   );
 }
