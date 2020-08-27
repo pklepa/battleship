738698c35c13c4c6eee99dba16516367c86a868c
klepa <pedro.klepa@gmail.com>
2020-08-27 15:15:37 -0300

    Updates

---
+++ precache-manifest.1b40e12e766ecd11a835db743f0a1112.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "978017ecd96e30bc8c10808eb8399825",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "ad36e62cd00d7413c7de",
+    "url": "/battleship/static/css/main.60e4c073.chunk.css"
+  },
+  {
+    "revision": "327747812e1a8a598ad0",
+    "url": "/battleship/static/js/2.30da2fca.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/battleship/static/js/2.30da2fca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad36e62cd00d7413c7de",
+    "url": "/battleship/static/js/main.186735af.chunk.js"
+  },
+  {
+    "revision": "1576b120f8ea920fbd40",
+    "url": "/battleship/static/js/runtime-main.9e463cc9.js"
+  },
+  {
+    "revision": "d96e83b828d12a3fa760e392a1cec7cd",
+    "url": "/battleship/static/media/port.d96e83b8.svg"
+  }
+]);
